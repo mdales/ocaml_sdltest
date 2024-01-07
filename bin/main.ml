039291c5dbc0d0dac9309f8840d8b7ab8636b377
Michael Dales <michael@digitalflapjack.com>
2024-01-07 14:19:07 +0000

    Minimal init

--- bin/main.ml
+++ bin/main.ml
@@ -1,13 +1,15 @@
 open Tsdl
 
-let init () = 
-  let a = Sdl.init Sdl.Init.(video + events) in
-  let rw = Result.bind a (fun () -> Sdl.create_window ~w:640 ~h:480 "SDL OpenGL" Sdl.Window.opengl) in
-  match Result.bind rw Sdl.create_renderer with
-  | Error e -> Error e
-  | Ok r -> Ok (Result.get_ok rw, r)
+let (>>=) = Result.bind
+let (>|=) v f = Result.map f v
 
-let main() = 
+let init () = 
+  Sdl.init Sdl.Init.(video + events) >>= fun () -> 
+  Sdl.create_window ~w:640 ~h:480 "SDL OpenGL" Sdl.Window.opengl >>= fun w -> 
+  Sdl.create_renderer w >|= 
+  fun r -> (w, r)
+
+let () = 
   match init () with
   | Error (`Msg e) -> Sdl.log "Init error: %s" e; exit 1
   | Ok (w, r) ->  
@@ -36,42 +38,3 @@ let main() =
           Sdl.destroy_window w;
           Sdl.quit ();
           exit 0
-
-
-let _main () = 
-  match Sdl.init Sdl.Init.(video + events) with
-  | Error (`Msg e) -> Sdl.log "Init error: %s" e; exit 1
-  | Ok () ->
-    match Sdl.create_window ~w:640 ~h:480 "SDL OpenGL" Sdl.Window.opengl with
-    | Error (`Msg e) -> Sdl.log "Create window error: %s" e; exit 1
-    | Ok w ->
-      match Sdl.create_renderer w with 
-      | Error (`Msg e) -> Sdl.log "Create renderer error: %s" e; exit 1
-      | Ok r ->
-        match Sdl.render_clear r with 
-        | Error (`Msg e) -> Sdl.log "Clear renderer error: %s" e; exit 1
-        | Ok () -> 
-          match Sdl.set_render_draw_color r 255 0 0 255 with
-          | Error (`Msg e) -> Sdl.log "Set color error: %s" e; exit 1
-          | Ok () -> 
-            match Sdl.render_draw_line r 10 10 100 100 with 
-            | Error (`Msg e) -> Sdl.log "draw line error: %s" e; exit 1
-            | Ok () -> 
-              Sdl.render_present r;
-
-              let rec loop () = (
-                let e = Sdl.Event.create () in
-                match Sdl.poll_event (Some e) with
-                | true -> (
-                  match Sdl.Event.(enum (get e typ)) with
-                  | `Quit -> ()
-                  | _ -> loop ()
-                )
-                | false -> loop()
-              ) in loop ();
-              Sdl.destroy_renderer r;
-              Sdl.destroy_window w;
-              Sdl.quit ();
-              exit 0
-
-let () = main ()
